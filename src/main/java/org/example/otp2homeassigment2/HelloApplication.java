8ba22922d9cf2ffd569be48647511983f6b9c54f
nimofarah <faranim01@gmail.com>
2024-11-03 18:05:56 +0200

    x

--- src/main/java/org/example/otp2homeassigment2/HelloApplication.java
+++ src/main/java/org/example/otp2homeassigment2/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.ResourceBundle;
 
 import java.io.IOException;
 
@@ -11,10 +12,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        fxmlLoader.setResources(ResourceBundle.getBundle("bundle1"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/org/example/otp2homeassigment2/HelloController.java
+++ src/main/java/org/example/otp2homeassigment2/HelloController.java
@@ -1,14 +1,65 @@
 package org.example.otp2homeassigment2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import java.util.ResourceBundle;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import java.util.Locale;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class HelloController {
+    public Button button1;
+
+    public Button button2;
+
+    public Button button3;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+
     }
+    public void initialize() {
+//        ResourceBundle bundle = ResourceBundle.getBundle("bundle2");
+//        button1.setText(bundle.getString("button1.text"));
+//        button2.setText(bundle.getString("button2.text"));
+//        button3.setText(bundle.getString("button3.text"));
+    }
+
+    public void setEN(ActionEvent actionEvent) throws IOException {
+        Locale l = new Locale("en", "UK");
+        loadView(l);
+    }
+
+    public void setIR(ActionEvent actionEvent) throws IOException {
+        Locale l = new Locale("fa", "IR");
+        loadView(l);
+    }
+
+    public void setJP(ActionEvent actionEvent) throws IOException {
+        Locale l = new Locale("ja", "JP");
+        loadView(l);
+    }
+
+    public void loadView(Locale locale) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        fxmlLoader.setResources(ResourceBundle.getBundle("bundle", locale));
+        try {
+            Parent root = fxmlLoader.load();
+            Stage s = (Stage) button1.getScene().getWindow();
+            s.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
